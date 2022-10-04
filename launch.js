c6f9a61659dfd10e5717c2f8ac02a729ffee8b26
Toni Iannucci <toni.iannucci@gmail.com>
2022-10-04 16:06:23 +0200

    Capsules'id
    Afficher l'id des capsules

--- launch.js
+++ launch.js
@@ -1,7 +1,12 @@
-function getData(){
-    fetch('https://api.spacexdata.com/v3/launches')
-        .then(res => res.json())
-        .then(data => console.log(data))
-    }
-    getData();
-    
+let url =`https://api.spacexdata.com/v3/capsules`;
+fetch(url).then((response) =>
+    response.json().then((data) => {
+        console.log(data);
+        let affichage = '<ul>';
+        for (let capsule of data) {
+            affichage += `<li>${capsule.capsule_serial}</li>`;
+        }
+        affichage += '</ul>';
+        document.querySelector("#capsules").innerHTML = affichage;
+    })
+);       
--- main.js
+++ main.js
@@ -2,6 +2,12 @@ function getData(){
     fetch('https://api.spacexdata.com/v3/capsules')
         .then(res => res.json())
         .then(data => console.log(data))
+        let affichage =`<ul>`;
+        for (let capsules of data) {
+            affichage += `<li>${capsules.capsule_serial}</li>`
+        }
+        affichage += `</ul>`; 
+        document.querySelector("#container").innerHTML = affichage;
     }
     getData();
     
