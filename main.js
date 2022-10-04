fac6a69a4bfb6d3a4f7482b70008247bcb1bf9b7
Toni Iannucci <toni.iannucci@gmail.com>
2022-10-04 16:40:36 +0200

    Update main.js
    Please

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-
-function getData(){
-fetch('https://api.spacexdata.com/v3/capsules/')
-    .then(res => res.json())
-    .then(data => console.log(data))
-}
-getData();
+let url =`https://api.spacexdata.com/v3/capsules`;
+fetch(url).then((response) =>
+    response.json().then((data) => {
+        console.log(data);
+        let affichage = '<ul>';
+        for (let capsule of data) {
+            affichage += `<li>${capsule.capsule_serial}</li>`;
+            affichage += `<li>${capsule.capsule_id}</li>`;
+            affichage += `<li>${capsule.status}</li>`;
+            affichage += `<li>${capsule.original_launch}</li>`;
+            affichage += `<li>${capsule.original_launch_unix}</li>`;
+        }
+        affichage += '</ul>';
+        document.querySelector("#capsules").innerHTML = affichage;
+    })
+);       
